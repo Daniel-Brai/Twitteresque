7a5d9620ac3950aabd8e55ca86d3a59c5bf5218a
Daniel Brai <braidaniel2001@gmail.com>
2023-02-04 04:14:15 +0100

    refactor: changed views logic and added testspecs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@
 
 # Controller for application 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   resources :tweets, only: :create
+  get 'dashboard', to: 'dashboard#index'
 end
